9e416246ba262ec37826ea183480eeb2a8d0779f
Lynn1207 <linlintoo2@gmail.com>
2020-06-28 03:29:04 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -280,7 +280,7 @@ def inference_local21(reshape):
 def inference_local22(reshape):
     dim = reshape.get_shape()[1]
      
-    with tf.variable_scope('local2_02_04_05_06_10') as scope:
+    with tf.variable_scope('local2_02_03_04_05_06_07_09_10_11_12') as scope:
         # Move everything into depth so we can perform a single matrix multiply.
         weights = _variable_with_weight_decay('weights', shape=[dim, 1024],
                                               stddev=0.04, wd=None)
@@ -295,7 +295,7 @@ def inference_local22(reshape):
 def inference_local23(reshape):
     dim = reshape.get_shape()[1]
      
-    with tf.variable_scope('local2_03_07_09_11_12') as scope:
+    with tf.variable_scope('local2_08') as scope:
         # Move everything into depth so we can perform a single matrix multiply.
         weights = _variable_with_weight_decay('weights', shape=[dim, 1024],
                                               stddev=0.04, wd=None)
