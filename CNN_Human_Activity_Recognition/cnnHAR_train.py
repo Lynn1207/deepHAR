8e3dbb1b79a43c1426f03f861d1d49744693d225
Lynn1207 <linlintoo2@gmail.com>
2020-07-15 19:18:28 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -89,19 +89,12 @@ def train():
     '''
     reshape1=cnnHAR.inference_cov11(signals)
     
-    local21=cnnHAR.inference_local21(reshape1)
-    
-    local31=cnnHAR.inference_local31(local21)
-    
-    local41=cnnHAR.inference_local41(local31)
-    
-    '''
-    logits1=cnnHAR.inference1(local41,'_01')
-    logits2=cnnHAR.inference1(local41,'_02')
-    logits3=cnnHAR.inference1(local41,'_03')
-    logits4=cnnHAR.inference1(local41,'_04')
-    logits5=cnnHAR.inference1(local41,'_05')
-    logits6=cnnHAR.inference1(local41,'_06')
+    logits1=cnnHAR.inference1(reshape1,'_01')
+    logits2=cnnHAR.inference1(reshape1,'_02')
+    logits3=cnnHAR.inference1(reshape1,'_03')
+    logits4=cnnHAR.inference1(reshape1,'_04')
+    logits5=cnnHAR.inference1(reshape1,'_05')
+    logits6=cnnHAR.inference1(reshape1,'_06')
     '''
     logits1=cnnHAR.inference_output1(local41)
     logits2=logits1
@@ -109,6 +102,7 @@ def train():
     logits4=logits1
     logits5=logits1
     logits6=logits1
+    '''
     
     loss1=cnnHAR.loss(logits1, labels,'_01')
     loss2=cnnHAR.loss(logits2, labels,'_02')
