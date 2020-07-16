b4c16237b835e42c8052fa7bc57cb89f09d702ed
Lynn1207 <linlintoo2@gmail.com>
2020-07-15 22:42:38 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -398,7 +398,7 @@ def inference_output1(local4):
     return softmax_linear
     
 def inference_output2(local4):
-    with tf.variable_scope('softmax_linear_03') as scope:
+    with tf.variable_scope('softmax_linear_03_05_06') as scope:
           weights = _variable_with_weight_decay('weights', [30, NUM_CLASSES],stddev=1/30.0, wd=None)
           biases = _variable_on_cpu('biases', [NUM_CLASSES],tf.constant_initializer(0.0))
           softmax_linear = tf.nn.softmax(tf.matmul(local4, weights)+biases,name=scope.name)
@@ -408,7 +408,7 @@ def inference_output2(local4):
     return softmax_linear
 
 def inference_output3(local4):
-    with tf.variable_scope('softmax_linear_04_05_06') as scope:
+    with tf.variable_scope('softmax_linear_04') as scope:
           weights = _variable_with_weight_decay('weights', [30, NUM_CLASSES],stddev=1/30.0, wd=None)
           biases = _variable_on_cpu('biases', [NUM_CLASSES],tf.constant_initializer(0.0))
           softmax_linear = tf.nn.softmax(tf.matmul(local4, weights)+biases,name=scope.name)
