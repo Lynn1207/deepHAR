6626b21556862ec1c69dbaaab910127e85776899
Lynn1207 <linlintoo2@gmail.com>
2020-07-16 00:33:57 -0400

    Update cnnHAR_eval.py

--- CNN_Human_Activity_Recognition/cnnHAR_eval.py
+++ CNN_Human_Activity_Recognition/cnnHAR_eval.py
@@ -278,15 +278,16 @@ def evaluate():
     
     # Build a Graph that computes the logits predictions from the
     # inference models
+    reshape1=cnnHAR.inference_cov11(signals)
+    reshape2=cnnHAR.inference_cov12(signals)
+    reshape3=cnnHAR.inference_cov13(signals)
     
-    
-    
-    logits1=cnnHAR.inference1(signals,'_01')
-    logits2=cnnHAR.inference1(signals,'_02')
-    logits3=cnnHAR.inference1(signals,'_03')
-    logits4=cnnHAR.inference1(signals,'_04')
-    logits5=cnnHAR.inference1(signals,'_05')
-    logits6=cnnHAR.inference1(signals,'_06')
+    logits1=cnnHAR.inference1(reshape1,'_01')
+    logits2=cnnHAR.inference1(reshape2,'_02')
+    logits3=cnnHAR.inference1(reshape3,'_03')
+    logits4=cnnHAR.inference1(reshape3,'_04')
+    logits5=cnnHAR.inference1(reshape3,'_05')
+    logits6=cnnHAR.inference1(reshape3,'_06')
     
     loss1=cnnHAR.loss(logits1, labels,'_01')
     loss2=cnnHAR.loss(logits2, labels,'_02')
