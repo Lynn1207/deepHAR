cea6be61554f13a3f173914ba2884095ca989150
Lynn1207 <linlintoo2@gmail.com>
2020-07-16 05:03:06 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -92,12 +92,16 @@ def train():
     '''
     reshape1=cnnHAR.inference_cov11(signals)
     
-    logits1=cnnHAR.inference1(reshape1,'_01')
-    logits2=cnnHAR.inference1(reshape1,'_02')
-    logits3=cnnHAR.inference1(reshape1,'_03')
-    logits4=cnnHAR.inference1(reshape1,'_04')
-    logits5=cnnHAR.inference1(reshape1,'_05')
-    logits6=cnnHAR.inference1(reshape1,'_06')
+    local21=cnnHAR.inference_local21(reshape1)
+    local22=cnnHAR.inference_local22(reshape1)
+    local23=cnnHAR.inference_local23(reshape1)
+    
+    logits1=cnnHAR.inference1(local21,'_01')
+    logits2=cnnHAR.inference1(local21,'_02')
+    logits3=cnnHAR.inference1(local21,'_03')
+    logits4=cnnHAR.inference1(local21,'_04')
+    logits5=cnnHAR.inference1(local22,'_05')
+    logits6=cnnHAR.inference1(local23,'_06')
     '''
     logits1=cnnHAR.inference_output1(local41)
     logits2=logits1
