605a3c66589b2a261e67668413922b6bbde4ab0b
Lynn1207 <linlintoo2@gmail.com>
2020-07-22 07:48:06 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -155,7 +155,7 @@ def _add_loss_summaries(total_loss,index):
 
     
 def inference_cov11(signals):
-    with tf.variable_scope('conv1_01_02_03_04_05_06_07_08_09_10_11_12') as scope:
+    with tf.variable_scope('conv1_01_02_03_04_05_06_07_08_09_10_11_12_13_14_15_16_17_18_19_20_21_22_23_24') as scope:
            kernel = _variable_with_weight_decay('weights',
                                                 shape=[ 20, 1, 64],
                                                 #shape=[3, 1, 128],
@@ -169,7 +169,7 @@ def inference_cov11(signals):
            print ('<<<<<<<<<<<<<<<<<<<<Shape of conv1 :',conv1.get_shape())
            
          # pool1
-    pool1 = tf.nn.max_pool1d(conv1, ksize=[1,3,1], strides=[1,3,1],padding='VALID',name='pool1_01_02_03_04_05_06_07_08_09_10_11_12')
+    pool1 = tf.nn.max_pool1d(conv1, ksize=[1,3,1], strides=[1,3,1],padding='VALID',name='pool1_01_02_03_04_05_06_07_08_09_10_11_12_13_14_15_16_17_18_19_20_21_22_23_24')
      
     reshape = tf.keras.layers.Flatten()(pool1)
     reshape = tf.cast(reshape, tf.float64)
