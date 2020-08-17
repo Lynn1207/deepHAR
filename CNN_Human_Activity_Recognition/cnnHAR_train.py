852108e5c8c8d40ee5f8150de92307e7b87558ba
Lynn1207 <linlintoo2@gmail.com>
2020-08-16 20:21:13 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -236,7 +236,7 @@ def train():
                #_LoggerHook2(),
                _LoggerHook4()],#,save_checkpoint_steps=5000
         config=tf.ConfigProto(
-            log_device_placement=log_device_placement),save_checkpoint_steps=max_steps) as mon_sess:
+            log_device_placement=log_device_placement),save_checkpoint_steps=log_frequency) as mon_sess:
       i=0
       while not mon_sess.should_stop():
 #        mon_sess = tfdbg.LocalCLIDebugWrapperSession(mon_sess)
