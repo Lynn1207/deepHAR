0b19cd118363b18d3aaaa5a0c22f5d6900f7fc35
Lynn1207 <linlintoo2@gmail.com>
2020-08-19 18:45:33 -0400

    Update cnnHAR_input.py

--- CNN_Human_Activity_Recognition/cnnHAR_input.py
+++ CNN_Human_Activity_Recognition/cnnHAR_input.py
@@ -29,7 +29,7 @@ import tensorflow.compat.v1 as tf
 # Process images of this size. Note that this differs from the original CIFAR
 # image size of 32 x 32. If one alters this number, then the entire model
 # architecture will change and any model would need to be retrained.
-SIGNAL_SIZE = 128
+SIGNAL_SIZE = 9
 channels = 1
 num=6
 
@@ -133,7 +133,7 @@ def distorted_inputs(data_dir, batch_size):
     images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
     labels: Labels. 1D tensor of [batch_size] size.
   """
-  filename = [os.path.join(data_dir, '2530_train.csv')]
+  filename = [os.path.join(data_dir, '0106_train.csv')]
   #if not tf.io.gfile.exists(filename):
     #raise ValueError('Failed to find file: ' + filename)
 
@@ -164,10 +164,10 @@ def distorted_inputs(data_dir, batch_size):
 def inputs(eval_data, data_dir, batch_size):
 
   if not eval_data:
-    filenames = [os.path.join(data_dir, '2530_train.csv')]
+    filenames = [os.path.join(data_dir, '0106_train.csv')]
     num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
   else:
-    filenames = [os.path.join(data_dir, '2530_test.csv')]
+    filenames = [os.path.join(data_dir, '0106_test.csv')]
     num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
   #if not tf.io.gfile.exists(filenames):
