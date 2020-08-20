c9b2e97404c6d96640a4205bce03434eaa6279b8
Lynn1207 <linlintoo2@gmail.com>
2020-08-20 15:59:53 -0400

    Update cnnHAR_eval.py

--- CNN_Human_Activity_Recognition/cnnHAR_eval.py
+++ CNN_Human_Activity_Recognition/cnnHAR_eval.py
@@ -241,12 +241,16 @@ def evaluate():
     local32=cnnHAR.inference_local32(local22)
     local33=cnnHAR.inference_local33(local23)
     
-    logits1=cnnHAR.inference1(local31,'_01')
-    logits2=cnnHAR.inference1(local32,'_02')
-    logits3=cnnHAR.inference1(local31,'_03')
-    logits4=cnnHAR.inference1(local31,'_04')
-    logits5=cnnHAR.inference1(local33,'_05')
-    logits6=cnnHAR.inference1(local31,'_06')
+    local41=cnnHAR.inference_local41(local31)
+    local42=cnnHAR.inference_local42(local32)
+    local43=cnnHAR.inference_local43(local33)
+    
+    logits1=cnnHAR.inference1(local41,'_01')
+    logits2=cnnHAR.inference1(local42,'_02')
+    logits3=cnnHAR.inference1(local41,'_03')
+    logits4=cnnHAR.inference1(local41,'_04')
+    logits5=cnnHAR.inference1(local43,'_05')
+    logits6=cnnHAR.inference1(local41,'_06')
     
     loss1=cnnHAR.loss(logits1, labels,'_01')
     loss2=cnnHAR.loss(logits2, labels,'_02')
