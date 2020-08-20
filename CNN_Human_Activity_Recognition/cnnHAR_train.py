9fc8bf420ad3215674446141bc73976cd4d02f56
Lynn1207 <linlintoo2@gmail.com>
2020-08-20 13:28:22 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -89,21 +89,16 @@ def train():
     local41=cnnHAR.inference_local41(local31)
     local42=cnnHAR.inference_local42(local32)
     local43=cnnHAR.inference_local43(local33)
-    
+    '''
     reshape1=cnnHAR.inference_cov11(signals)
+    reshape2=cnnHAR.inference_cov12(signals)
     
-    local21=cnnHAR.inference_local21(reshape1)
-    
-    local31=cnnHAR.inference_local31(local21)
-    
-    local41=cnnHAR.inference_local41(local31)
-    '''                               
-    logits1=cnnHAR.inference1(signals,'_01')
-    logits2=cnnHAR.inference1(signals,'_02')
-    logits3=cnnHAR.inference1(signals,'_03')
-    logits4=cnnHAR.inference1(signals,'_04')
-    logits5=cnnHAR.inference1(signals,'_05')
-    logits6=cnnHAR.inference1(signals,'_06')
+    logits1=cnnHAR.inference1(reshape1,'_01')
+    logits2=cnnHAR.inference1(reshape2,'_02')
+    logits3=cnnHAR.inference1(reshape1,'_03')
+    logits4=cnnHAR.inference1(reshape1,'_04')
+    logits5=cnnHAR.inference1(reshape1,'_05')
+    logits6=cnnHAR.inference1(reshape1,'_06')
     
     '''
     reshape1=cnnHAR.inference_cov11(signals)
