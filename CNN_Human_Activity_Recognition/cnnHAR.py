29d49e0f91b84b0e1b0faad1eb6fd7f2d76bbf7a
Lynn1207 <linlintoo2@gmail.com>
2020-08-20 23:26:15 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -447,8 +447,8 @@ def inference_output6(local4):
     
     return softmax_linear
     
-def inference1(local4,index):
-    '''
+def inference1(signals,index):
+    
     with tf.variable_scope('conv1'+index) as scope:
            kernel = _variable_with_weight_decay('weights',
                                                 shape=[ 20, 1, 64],
@@ -497,7 +497,7 @@ def inference1(local4,index):
         local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
         print ('!!!!!!!!!!!!!!!Shape of local4 :', local4.get_shape())#256
         _activation_summary(local4)
-    '''
+    
     with tf.variable_scope('softmax_linear'+index) as scope:
           weights = _variable_with_weight_decay('weights', [30, NUM_CLASSES],stddev=1/30.0, wd=None)
           biases = _variable_on_cpu('biases', [NUM_CLASSES],tf.constant_initializer(0.0))
