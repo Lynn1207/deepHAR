c617d7d16848c40aef70864cab0eeb923e6a3c80
Lynn1207 <linlintoo2@gmail.com>
2021-01-27 03:06:30 -0500

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -186,7 +186,7 @@ def train():
                         'sec/batch)')
           '''
           format_str = ('%s: step %d, loss%d = %.8f')
-          print (format_str % (datetime.now(), self._step+1, int(self._step%(num*7)/7), run_values.results))
+          print (format_str % (datetime.now(), self._step+1, int(self._step%(num*7)/7)+1, run_values.results))
          
     class _LoggerHook2(tf.train.SessionRunHook):
       """Logs signals."""
